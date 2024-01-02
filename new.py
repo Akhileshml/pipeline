a4c597b9ac96d16878cf65fd7b5d1d48d51c630d
Akhileshml <akhileshmony@gmail.com>
2024-01-02 11:13:53 +0530

    changed

--- new.py
+++ new.py
@@ -1,3 +1,5 @@
-a=input("enter your name")
-b=input("enter your age")
-print(a,'age is ',b)
+try:
+    user_input = input("Enter something: ")
+    print("You entered:", user_input)
+except EOFError:
+    print("An EOFError occurred.")
